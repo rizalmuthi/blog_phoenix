36523b2695cbc78656fceabcb88470d3c855e6fb
Rizal Muthi <rizalmuthi@gmail.com>
2016-01-23 18:28:13 +0800

    Add add_comment action

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -2,8 +2,10 @@ defmodule BlogPhoenix.PostController do
   use BlogPhoenix.Web, :controller
 
   alias BlogPhoenix.Post
+  alias BlogPhoenix.Comment
 
   plug :scrub_params, "post" when action in [:create, :update]
+  plug :scrub_params, "comment" when action in [:add_comment]
 
   def index(conn, _params) do
     posts = Repo.all(Post)
@@ -63,5 +65,20 @@ defmodule BlogPhoenix.PostController do
     conn
     |> put_flash(:info, "Post deleted successfully.")
     |> redirect(to: post_path(conn, :index))
+  end
+
+  def add_comment(conn, %{"comment" => comment_params, "post_id" => post_id}) do
+    changeset = Comment.changeset(%Comment{}, Map.put(comment_params, "post_id", post_id))
+    post = Post |> Repo.get(post_id) |> Repo.preload([:comments])
+
+    if changeset.valid? do
+      Repo.insert(changeset)
+
+      conn
+      |> put_flash(:info, "Comment added.")
+      |> redirect(to: post_path(conn, :show, post))
+    else
+      render(conn, "show.html", post: post, changeset: changeset)
+    end
   end
 end
